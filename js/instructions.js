78d825520abe8d944db52a418afbc08143eeb237
yvonne <yvqu0001@stud.ek.dk>
2025-11-03 08:43:34 +0100

    infographic text update

--- js/instructions.js
+++ js/instructions.js
@@ -10,6 +10,12 @@ function eyesRed() {
   document.querySelector("#eyes").classList.remove("hide");
   document.querySelector("#teeth").classList.add("hide");
   document.querySelector("#nails").classList.add("hide");
+  document.querySelector("#feature").textContent = "Red eyes";
+  document.querySelector("#danger-level").textContent = "Danger level: 4";
+  document.querySelector("#efficiency").textContent =
+    "Commonly seen in real and fake humans, beware. If only this symptom is showing be cautious, but it is not definitive on its own. Recent intake of caffeine can impact test results.";
+  document.querySelector("#requirement").innerHTML =
+    "<strong>Natural Reasons</strong><ul><li>Lack of sleep</li><li>Dry air</li><li>Allergies</li><li>Contacts</li></ul>";
 }
 
 document.querySelector("#teethbtn").addEventListener("click", teethPerfect);
@@ -18,6 +24,12 @@ function teethPerfect() {
   document.querySelector("#teeth").classList.remove("hide");
   document.querySelector("#eyes").classList.add("hide");
   document.querySelector("#nails").classList.add("hide");
+  document.querySelector("#feature").textContent = "Perfect teeth";
+  document.querySelector("#danger-level").textContent = "Danger level: 6";
+  document.querySelector("#efficiency").textContent =
+    "Perfectly aligned white teeth. Humans tend to have slight yellow tinted teeth with crooked imperfections. Beware of recent dentist appointments. Recent smoking can impact test results.";
+  document.querySelector("#requirement").innerHTML =
+    "<strong>Natural Reasons</strong><ul><li>Recent dental appointment</li><li>Dentures</li><li>Past braces</li><li>No smoking</li></ul>";
 }
 
 document.querySelector("#nailsbtn").addEventListener("click", nailsDirty);
@@ -26,4 +38,10 @@ function nailsDirty() {
   document.querySelector("#nails").classList.remove("hide");
   document.querySelector("#eyes").classList.add("hide");
   document.querySelector("#teeth").classList.add("hide");
+  document.querySelector("#feature").textContent = "Dirty nails";
+  document.querySelector("#danger-level").textContent = "Danger level: 7";
+  document.querySelector("#efficiency").textContent =
+    "Unkempt nails are a severe sign of a fake human; proceed with caution. Recent handwashing can impact the test results.";
+  document.querySelector("#requirement").innerHTML =
+    "<strong>Natural Reasons</strong><ul><li>Dirty work ex. gardening</li><li>Biting or picking nails</li><li>Longer nails</li><li>Sweaty hands</li></ul>";
 }
